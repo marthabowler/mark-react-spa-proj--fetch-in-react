c69c4e72e2d475f0bdc96fbba8482cc6004015a3
Martha Bowler <mbowler133@gmail.com>
2021-11-02 16:58:01 +0000

    Add dog history

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,100 @@
 import { useState } from "react";
 
-interface Joke {
-  id: number;
-  type: string;
-  setup: string;
-  punchline: string;
+interface Dog {
+  message: string;
+  status: string;
 }
 
 function App() {
-  const [joke, setJoke] = useState<Joke>();
+  const [dog, setDog] = useState<Dog[]>([]);
 
-  const handleGetJoke = async () => {
-    const response = await fetch(
-      "https://jokestemp.neillbogie.repl.co/jokes/general/random"
-    );
-    const jsonBody: Joke[] = await response.json();
-    setJoke(jsonBody[0]);
+  const handleGetDog = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((jsonBody: Dog) => setDog((dog) => [...dog, jsonBody]));
   };
 
-  // const handleGetJoke = () => {
-  //   fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
-  //     .then((response) => response.json())
-  //     .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
-  // };
-
-  if (joke) {
+  if (dog.length > 0) {
     return (
       <div>
-        <h1>Joke app</h1>
-        <details>
-          <summary>{joke.setup}</summary>
-          <p>{joke.punchline}</p>
-        </details>
+        <h1>Dog app</h1>
+        <img src={dog[dog.length - 1].message} alt={"#"} width="300px" />
         <hr />
-        <button onClick={handleGetJoke}>Get another joke</button>
+        <button onClick={handleGetDog}>Get another dog</button>
+        <p>
+          your last dogs:{" "}
+          {dog.slice(-6, -1).map((dg, index) => (
+            <li key={index}>
+              <img src={dg.message} alt={"#"} width="50px" />
+            </li>
+          ))}
+        </p>
       </div>
     );
   } else {
     return (
       <div>
-        <h1>Joke app</h1>
+        <h1>Dog app</h1>
         <p>
           Click the button to trigger a <code>fetch</code> that gets a random
-          joke from an API!
+          dog from an API!
         </p>
-        <button onClick={handleGetJoke}>Get joke</button>
+        <button onClick={handleGetDog}>Get dog</button>
       </div>
     );
   }
 }
 
 export default App;
+
+// interface Joke {
+//   id: number;
+//   type: string;
+//   setup: string;
+//   punchline: string;
+// }
+
+// function App() {
+//   const [joke, setJoke] = useState<Joke>();
+
+//   // const handleGetJoke = async () => {
+//   //   const response = await fetch(
+//   //     "https://jokestemp.neillbogie.repl.co/jokes/general/random"
+//   //   );
+//   //   const jsonBody: Joke[] = await response.json();
+//   //   setJoke(jsonBody[0]);
+//   // };
+
+//   const handleGetJoke = () => {
+//     fetch("https://jokestemp.neillbogie.repl.co/jokes/general/random")
+//       .then((response) => response.json())
+//       .then((jsonBody: Joke[]) => setJoke(jsonBody[0]));
+//   };
+
+//   if (joke) {
+//     return (
+//       <div>
+//         <h1>Joke app</h1>
+//         <details>
+//           <summary>{joke.setup}</summary>
+//           <p>{joke.punchline}</p>
+//         </details>
+//         <hr />
+//         <button onClick={handleGetJoke}>Get another joke</button>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <h1>Joke app</h1>
+//         <p>
+//           Click the button to trigger a <code>fetch</code> that gets a random
+//           joke from an API!
+//         </p>
+//         <button onClick={handleGetJoke}>Get joke</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
